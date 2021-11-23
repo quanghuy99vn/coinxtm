731d9fada1d4efa003d5914f7e9ba1178da64960
quanghuy99vn <67700060+quanghuy99vn@users.noreply.github.com>
2021-11-23 22:41:16 +0700

    Update miner-raptoreum.sh

--- cpuminer-opt-linux/miner-raptoreum.sh
+++ cpuminer-opt-linux/miner-raptoreum.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 while [ 1 ]; do
-./cpuminer-sse2 -a gr -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u WALLET.WORKER_NAME
+./cpuminer-sse2 -a gr -o stratum+tcps://asia.flockpool.com:5555 -u RJxyR1mveXMAW1h1qKqUj3T3Hjz2t8YxdF.WORKER_NAME
 sleep 5
 done
